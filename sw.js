26a34e99178c5a795d6195b6959bfc82e939a006
ikram-cyber <59032248+ikram-cyber@users.noreply.github.com>
2026-03-27 05:02:18 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,2 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('kas-santai-v1').then((cache) => cache.addAll([
-      './index.html',
-      './manifest.json'
-    ]))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
+self.addEventListener('install', (e) => { console.log('SW Ready'); });
+self.addEventListener('fetch', (e) => {});
